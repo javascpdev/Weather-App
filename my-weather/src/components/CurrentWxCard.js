4aeef0629e9b817588de1e5796616ef673021ebe
javascpdev <kingpower1990115@gmail.com>
2020-02-13 10:41:51 -0500

    Pulling in more data to dashboard, working on dynamically rendering icons

--- my-weather/src/components/CurrentWxCard.js
+++ my-weather/src/components/CurrentWxCard.js
@@ -5,10 +5,18 @@ const CurrentWxCard = (props) => {
     return (
         <div>
             {props.data.map(info => {
-                return (
+                let weather = info.weather.icon
+                console.log(weather)
+                return ( 
                     <div>
+                        <div class="weather-icon">
+                            <img src={`../../icons/._${weather}.png`} alt="wx-icon"/>
+                        </div>
                         <h1>Location: {info.city_name}, {info.state_code}, {info.country_code}</h1>
-                        <p>Current Temp: {info.temp}</p>
+                        <p>Current Temp: {info.temp}F</p>
+                        <p>Dewpoint: {info.dewpt}F</p>
+                        <p>Relative Humidity: {info.rh}%</p>
+                        <p>Wind: {info.wind_spd}MPH out of the {info.wind_cdir}</p>
                     </div>
                 )
             })}
