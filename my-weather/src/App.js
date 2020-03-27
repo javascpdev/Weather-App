557fbfc0927df02d8dffb2768c4206f8ba9ab4fc
javascpdev <kingpower1990115@gmail.com>
2020-03-27 17:59:58 -0400

    Using useposition hook to retrieve users location on page load

--- my-weather/src/App.js
+++ my-weather/src/App.js
@@ -1,35 +1,28 @@
 // Base imports
-
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // Components imports
-
-import Opening from './components/Opening';
-import Dashboard from './components/Dashboard';
+import Opening from "./components/Opening";
+import Dashboard from "./components/Dashboard";
 // React Router imports
-
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 // Material UI imports
-
-import { createMuiTheme } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/styles';
-
+import { createMuiTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
 
 const openingtheme = createMuiTheme({
   typography: {
-    fontFamily: [
-    'Handlee, cursive'
-    ]
-  },
-})
+    fontFamily: ["Handlee, cursive"]
+  }
+});
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={openingtheme}>
-        <Route exact path='/' component={Opening} />
+        <Route exact path="/" component={Opening} />
       </ThemeProvider>
-      <Route path='/dashboard' component={Dashboard} />
+      <Route path="/dashboard" component={Dashboard} />
     </div>
   );
 }
--- my-weather/src/components/Dashboard.js
+++ my-weather/src/components/Dashboard.js
@@ -1,60 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { usePosition } from "use-position";
 
 // Component Imports
-import CurrentWxCard from './CurrentWxCard';
+import CurrentWxCard from "./CurrentWxCard";
 
 const Dashboard = () => {
-    const [currentWx, setCurrentWx] = useState([]);
-    const [userLocation, setUserLocation] = useState({
-        postal_code: ''
-    });
+  const [currentWx, setCurrentWx] = useState([]);
+  const { latitude, longitude, error } = usePosition();
 
-    const handleLocationChange = event => {
-        event.preventDefault();
-        setUserLocation({[event.target.name]: event.target.value})
-    };
-
-    // function cToF(celsius) {
-    //     let cTemp = celsius
-    //     let cToFahr = cTemp * 9 / 5 + 32;
-    //     let message = cTemp +'\xB0C is' + cToFahr + ' \xB0F.';
-    //         console.log(message)
-    // }
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        axios.get(`https://api.weatherbit.io/v2.0/current?postal_code=${userLocation.postal_code}&key=cd48f97735b04b7289b16e1613d1d2f1&units=i`)
+  useEffect(() => {
+    if (latitude && longitude && !error) {
+      axios
+        .get(
+          `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=cd48f97735b04b7289b16e1613d1d2f1&units=i`
+        )
         .then(res => {
-            console.log(res)
-            setCurrentWx(res.data.data)
-        })   
+          console.log(res);
+          setCurrentWx(res.data.data);
+        });
     }
-    console.log(currentWx)
-    
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                label="Zip Code"
-                name="postal_code"
-                type="text"
-                value={userLocation.postal_code} 
-                onChange={handleLocationChange}
-                />
-            <button type="Submit">Get Weather</button>
-            </form>
-            <div>
-                {currentWx.map(info => {
-                    return (
-                        <CurrentWxCard 
-                        data={currentWx}
-                        />
-                    )
-                })}
-            </div>
-        </div>
-    );
+  }, [latitude, longitude, error]);
+
+  return (
+    <div>
+      <div>
+        {currentWx.map(info => {
+          return <CurrentWxCard data={currentWx} key={info.datetime} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
